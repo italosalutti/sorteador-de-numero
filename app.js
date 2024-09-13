d4779f53be2965c590f3b5311f670ff1a429c0e9
Italo Salutti <salutti@gmail.com>
2024-09-13 14:52:39 -0300

    sorteador inicial

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear(){
+    let quantidade = document.getElementById('Quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade : ${quantidade}`);
+    alert(`Do número: ${quantidade}`);
+    alert(`Até o número : ${quantidade}`);
+
+}
